6e6c6ed63a7c38f7c8af533df969a775f4341827
Jaime Marques <mrthesoulz@gmail.com>
2014-01-19 16:41:55 +0000

    Test Release
    Auto Taunts, Fix Commands, Items

--- libs/lib.lua
+++ libs/lib.lua
@@ -10,6 +10,12 @@ function GetVer()
 	return mts:message('MrTheSoulz Version: 2.1.0')
 end
 
+-- //////////////////////-----------------------------------------CONDITIONS-----------------------------------//////////////////////////////
+
+ProbablyEngine.condition.register("talent", function(index)
+	return select(5, GetTalentInfo(index)) or false
+end)
+
 -- //////////////////////-----------------------------------------NOTIFICATIONS-----------------------------------//////////////////////////////
 	
 	-- Chat Overlay originally made by Sheuron
@@ -36,8 +42,152 @@ end
 	  self:Show() 
 	end
 
--- //////////////////////-----------------------------------------CONDITIONS-----------------------------------//////////////////////////////
 
-ProbablyEngine.condition.register("talent", function(index)
-	return select(5, GetTalentInfo(index)) or false
-end)
+-- //////////////////////-----------------------------------------Taunts-----------------------------------//////////////////////////////
+
+function mts.bossTaunt()
+
+    if UnitGroupRolesAssigned("player") == "TANK" and IsInRaid() then
+        local otherTank
+            for i = 1, GetNumGroupMembers() do
+                local other = "raid" .. i
+                    if not otherTank and not UnitIsUnit("player", other) and UnitGroupRolesAssigned(other) == "TANK" then
+                        otherTank = other
+                    end
+    end
+
+    if otherTank and not UnitIsDeadOrGhost(otherTank) then
+        for j = 1, 4 do
+            local boss = UnitID(bossID)
+            local bossID = "boss" .. j
+
+                if boss == 71543 then -- Immersus
+                    if UnitDebuff(otherTank, "Corrosive Blast") and not UnitDebuff("player", "Corrosive Blast") then
+                        ProbablyEngine.dsl.parsedTarget = bossID
+                    	return true
+                	end 
+
+                elseif boss == 72276 then -- Norushen
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Self Doubt")
+	                if debuffName and debuffCount > 2 and not UnitDebuff("player", "Self Doubt") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	                end 
+
+                elseif boss == 71734 then -- Sha of Pride
+                    if UnitDebuff(otherTank, "Wounded Pride") and not UnitDebuff("player", "Wounded Pride") then
+                        ProbablyEngine.dsl.parsedTarget = bossID
+                    	return true
+                	end
+
+                elseif boss == 72249 then -- Galakras
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Flames of Galakrond")
+	                if debuffName and debuffCount > 2 and not UnitDebuff("player", "Flames of Galakrond") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	                end
+
+                elseif boss == 71466 then -- Iron Juggernaut
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Ignite Armor")
+	                if debuffName and debuffCount > 1 and not UnitDebuff("player", "Ignite Armor") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	               	end
+
+                elseif boss == 71859 then -- Kor'kron Dark Shaman
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Froststorm Strike")
+	                if debuffName and debuffCount > 4 and not UnitDebuff("player", "Froststorm Strike") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	                end
+
+                elseif boss == 71515 then -- General Nazgrim
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Sundering Blow")
+	                if debuffName and debuffCount > 2 and not UnitDebuff("player", "Sundering Blow") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	                end
+
+                elseif boss == 71454 then -- Malkorok
+                local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Fatal Strike")
+	                if debuffName and debuffCount > 11 and not UnitDebuff("player", "Fatal Strike") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	               	end
+
+                elseif boss == 71529 then -- Thok the Bloodsthirsty
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Panic")
+	                if debuffName and debuffCount > 2 and not UnitDebuff("player", "Panic") then
+	                    ProbablyEngine.dsl.parsedTarget = bossID
+	                    return true
+	               	end
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Acid Breath")
+                    if debuffName and debuffCount > 2 and not UnitDebuff("player", "Acid Breath") then
+                        ProbablyEngine.dsl.parsedTarget = bossID
+                        return true
+                    end 
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Freezing Breath")
+                    if debuffName and debuffCount > 2 and not UnitDebuff("player", "Freezing Breath") then
+                        ProbablyEngine.dsl.parsedTarget = bossID
+                        return true
+                    end 
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Scorching Breath")
+                    if debuffName and debuffCount > 2 and not UnitDebuff("player", "Scorching Breath") then
+                        ProbablyEngine.dsl.parsedTarget = bossID
+                        return true
+                    end
+
+                elseif boss == 71504 then -- Siegecrafter Blackfuse
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Electrostatic Charge")
+                    if debuffName and debuffCount > 3 and not UnitDebuff("player", "Electrostatic Charge") then
+                        robablyEngine.dsl.parsedTarget = bossID
+                        return true
+                   	end
+
+                elseif boss == 71865 then -- Garrosh Hellscream
+                    local debuffName, _, _, debuffCount = UnitDebuff(otherTank, "Gripping Despair")
+                    if debuffName and debuffCount > 2 and not UnitDebuff("player", "Gripping Despair") then
+                        ProbablyEngine.dsl.parsedTarget = bossID
+                        return true
+                    end
+
+                end -- End Boss's Table
+            end
+        end
+    end
+    	return false
+end
+
+-- //////////////////////-----------------------------------------Boss's-----------------------------------//////////////////////////////
+
+--mts.bosses = {
+
+-- Soo Bosses
+--	71543, -- Immersus
+--	72276, -- Norushen
+--	71734, -- Sha of Pride
+--	72249, -- Galakras
+--	71466, -- Iron Juggernaut
+--	71859, -- Kor'kron Dark Shaman
+--	71515, -- General Nazgrim
+--	71454, -- Malkorok
+--	71529, -- Thok the Bloodsthirsty
+--	71504, -- Siegecrafter Blackfuse
+--	71865  -- Garrosh Hellscream
+--
+--}
+
+--local mts.bosses = { 71543, 72276, 71734, 72249, 71466, 71859, 71515, 71454, 71529, 71504, 71865 }
+--function mts.StopIfBoss()
+--    for i = 1, 4 do
+--      local bossTarget = "boss" .. i
+--      local bossID = UnitID(bossTarget)
+--      if table.contains(mts.bosses, bossID) then
+--          return false
+--      end
+--    end
+--    return true
+--end
+
+-- //////////////////////-----------------------------------------Register Lib-----------------------------------//////////////////////////////
+ProbablyEngine.library.register('mts', mts)
--- rotations/paladin/protection.lua
+++ rotations/paladin/protection.lua
@@ -15,78 +15,106 @@ local lib = function()
 	
 -- ////////////////////////-----------------------------------------COMMANDS-----------------------------------//////////////////////////////
 
-	local mtsPalaProt = {
-		con = true,
-		seal = true,
-		Whisper = true, -- "!!!!Change this to true if you want it ON by default!!!"
-		ChatOverlay = true
-	}
-	
-	function mtsPalaProt.GetCon()
-		return mtsPalaProt.con
-	end
+-- Table
+local mtscommands = {
+	con = true,
+	seals = true,
+	whisper = true, -- "!!!!Change this to true if you want it ON by default!!!"
+	chat = true
+}
 
-	function mtsPalaProt.Getseal()
-		return mtsPalaProt.seal
-	end
+-- Return's	
+function mts.GetCon()
+	return mtscommands.con
+end
 
-	function mtsPalaProt.GetWhisper()
-		return mtsPalaProt.Whisper
-	end
+function mts.GetSeals()
+	return mtscommands.seals
+end
 
-	function mtsPalaProt.GetChatOverlay()
-		return mtsPalaProt.ChatOverlay
-	end
-	
-	ProbablyEngine.command.register('mts', function(msg, box)
-	local command, text = msg:match("^(%S*)%s*(.-)$")
+function mts.Getwhisper()
+	return mtscommands.whisper
+end
+
+function mts.Getchat()
+	return mtscommands.chat
+end
+
+-- Listener	
+ProbablyEngine.command.register('mts', function(msg, box)
+local command, text = msg:match("^(%S*)%s*(.-)$")
 		
-		if command == 'ver' or command == 'version' then
-			GetVer()
+	-- Dispaly Version
+	if command == 'ver' or command == 'version' then
+		GetVer()
+	end
+		
+	-- Disable Rotation Using consecration
+	if command == 'con' then
+	mtscommands.con = not mtscommands.con
+		if mtscommands.con then
+			mts:message('*consecration Enabled.*')
+		else
+			mts:message('*consecration Disabled*.')
 		end
+	end
 		
-		-- Disable Rotation Using consecration
-			if command == 'con' then
-			mtsPalaProt.con = not mtsPalaProt.con
-				if mtsPalaProt.con then
-					mts:message('*consecration Enabled.*')
-				else
-					mts:message('*consecration Disabled*.')
-				end
-			end
-		
-		-- Disable Rotation Changing seals	
-			if command == 'seals' then
-			mtsPalaProt.seal = not mtsPalaProt.seal
-				if mtsPalaProt.seal then
-					mts:message('*seals Enabled.*')
-				else
-					mts:message('*seals Disabled.*')
-				end
-			end
+	-- Disable Rotation Changing seals	
+	if command == 'seals' then
+	mtscommands.seals = not mtscommands.seals
+		if mtscommands.seals then
+			mts:message('*seals Enabled.*')
+		else
+			mts:message('*seals Disabled.*')
+		end
+	end
 
-		-- Disable Notes
-			if command == 'chat' then
-			mtsPalaProt.ChatOverlay = not mtsPalaProt.ChatOverlay
-				if mtsPalaProt.ChatOverlay then
-					mts:message('*Chat Overlay Enabled.*')
-				else
-					mts:message('*Chat Overlay Disabled.*')
-				end
-			end
+	-- Disable Notes
+	if command == 'chat' then
+	mtscommands.chat = not mtscommands.chat
+		if mtscommands.chat then
+			mts:message('*Chat Overlay Enabled.*')
+		else
+			mts:message('*Chat Overlay Disabled.*')
+		end
+	end
 
-		-- Allow Whispers
-			if command == 'ws' or command == 'Whisper' or command == 'whisper' then
-				mtsPalaProt.Whisper = not mtsPalaProt.Whisper
-				if mtsPalaProt.Whisper then
-					mts:message("*Whispers: ON*")
-				else
-					mts:message("*Whispers: OFF*")
-				end
-			end
+	-- Allow Whispers
+	if command == 'ws' or command == 'Whisper' or command == 'whisper' then
+	mtscommands.whisper = not mtscommands.whisper
+		if mtscommands.whisper then
+			mts:message("*Whispers: ON*")
+		else
+			mts:message("*Whispers: OFF*")
+		end
+	end
 
-	end)
+end)
+-- //////////////////////-----------------------------------------ITEMS-----------------------------------//////////////////////////////
 
+-- Master Health Potion
+function mts.HealthPot()
+	if GetItemCount(76097) > 1 
+	and GetItemCooldown(76097) == 0 
+	and ProbablyEngine.condition["modifier.cooldowns"] then 
+		return true
+	end
+
+	-- If Dont have it or its in cooldown
+	return false
+end
+
+-- Kafa Press // Not working right... //
+--function mts.KafaPress()
+--	if GetItem(86125)
+--	and GetItemCooldown(86125) == 0 
+--	and ProbablyEngine.condition["modifier.cooldowns"] then 
+--		return true
+--	end
+--
+--	-- If Dont have it or its in cooldown
+--	return false
+--end
 
 -- //////////////////////-----------------------------------------NOTIFICATIONS-----------------------------------//////////////////////////////
 
@@ -96,63 +124,87 @@ local lib = function()
 	local spellId = select(12, ...)
 	local tname = UnitName("target")
 	if source ~= UnitGUID("player") then return false end
-
-		if mtsPalaProt.GetChatOverlay() then
 	
-			if event == "SPELL_CAST_SUCCESS" then
+		if event == "SPELL_CAST_SUCCESS" then
 
-				-- Keybinds
-					if spellId == 114158 then
-						mts:message("*Casted Light´s Hammer*")
-					end
-
-				-- Stuns
-					if spellId == 105593 then
-						mts:message("*Stunned Target*")
-					end
-					if spellId == 853 then
-						mts:message("*Stunned Target*")
-					end
-		
-				-- Free Yourself
-					if spellId == 1044 then
-						mts:message("*Casted Hand of Freedom*")
-					end
-					
-				-- Cooldowns
-					if spellId == 633 then
-						mts:message("*Casted Lay On Hands*")
-						if mtsPalaProt.GetWhisper() then
-							RunMacroText("/w "..tname.." MSG: Casted Lay On Hands on you.")
-						end
-					end
-					if spellId == 31821 then
-						mts:message("*Casted Devotion Aura*")
-					end
-					if spellId == 31884 then
-						mts:message("*Casted Avenging Wrath*")
-					end
-					if spellId == 86669 then
-						mts:message("*Casted Guardian of Ancient Kings*")
-					end
-					if spellId == 6940 then
-						mts:message("*Casted Hand of Sacrifice*")
-						if mtsPalaProt.GetWhisper() then
-							RunMacroText("/w "..tname.." MSG: Casted Hand of Sacrifice on you.")
-						end
-					end
-					if spellId == 1044 then
-						mts:message("*Casted Hand of Sacrifice*")
-						if mtsPalaProt.GetWhisper() then
-							RunMacroText("/w "..tname.." MSG: Casted Hand of Sacrifice on you.")
-						end
-					end
-
+		-- Keybinds
+			if spellId == 114158 then
+				if mts.Getchat() then
+				mts:message("*Casted Light´s Hammer*")
+				end
 			end
-		end
+
+		-- Stuns
+			if spellId == 105593 then
+				if mts.Getchat() then
+					mts:message("*Stunned Target*")
+				end
+			end
+
+			if spellId == 853 then
+				if mts.Getchat() then
+					mts:message("*Stunned Target*")
+				end
+			end
+		
+		-- Free Yourself
+			if spellId == 1044 then
+				if mts.Getchat() then
+					mts:message("*Casted Hand of Freedom*")
+				end
+			end
+					
+		-- Cooldowns
+			if spellId == 633 then
+				if mts.Getchat() then
+					mts:message("*Casted Lay On Hands*")
+				end
+				if mts.Getwhisper() then
+					RunMacroText("/w "..tname.." MSG: Casted Lay On Hands on you.")
+				end
+			end
+
+			if spellId == 31821 then
+				if mts.Getchat() then
+					mts:message("*Casted Devotion Aura*")
+				end
+			end
+
+			if spellId == 31884 then
+				if mts.Getchat() then
+					mts:message("*Casted Avenging Wrath*")
+				end
+			end
+
+			if spellId == 86669 then
+				if mts.Getchat() then
+					mts:message("*Casted Guardian of Ancient Kings*")
+				end
+			end
+
+			if spellId == 6940 then
+				if mts.Getchat() then
+					mts:message("*Casted Hand of Sacrifice*")
+				end
+				if mts.Getwhisper() then
+					RunMacroText("/w "..tname.." MSG: Casted Hand of Sacrifice on you.")
+				end
+			end
+
+			if spellId == 1044 then
+				if mts.Getchat() then
+					mts:message("*Casted Hand of Sacrifice*")
+				end
+				if mts.Getwhisper() then
+					RunMacroText("/w "..tname.." MSG: Casted Hand of Sacrifice on you.")
+				end
+			end
+
+		end -- Ends Table
 	end)
 
-ProbablyEngine.library.register('mtsPalaProt', mtsPalaProt)
+-- //////////////////////-----------------------------------------Register Lib-----------------------------------//////////////////////////////
+ProbablyEngine.library.register('mts', mts)
 
 end
 
@@ -162,24 +214,12 @@ end
 local Buffs = {
 
 	-- Buffs
-		{ "19740", { -- Blessing of Might
-			"!player.buff(19740).any",
-			"!player.buff(116956).any",
-			"!player.buff(93435).any",
-			"!player.buff(128997).any",
-			"!toggle.buff"
-		}, nil },
-		{ "20217", { -- Blessing of Kings
-			"!player.buff(20217).any",
-			"!player.buff(115921).any",
-			"!player.buff(1126).any",
-			"!player.buff(90363).any",
-			"!player.buff(69378).any",
-			"toggle.buff"
-		}, nil },
+		{ "19740", { "!player.buff(19740).any", "!player.buff(116956).any", "!player.buff(93435).any", "!player.buff(128997).any", "!toggle.buff" }, nil }, -- Blessing of Might
+		{ "20217", { "!player.buff(20217).any", "!player.buff(115921).any", "!player.buff(1126).any", "!player.buff(90363).any", "!player.buff(69378).any", "toggle.buff" }, nil }, -- Blessing of Kings
 		{ "25780", "!player.buff(25780).any" }, -- Fury
 		
 }
+
 -- ////////////////////////-----------------------------------------END BUFFS-----------------------------------//////////////////////////////
 
 
@@ -189,91 +229,74 @@ local inCombat = {
 		{ "105593", "modifier.control", "target"}, -- Fist of Justice
 		{ "853", "modifier.control", "target"}, -- Hammer of Justice
 		{ "114158", "modifier.shift", "ground"}, -- Light´s Hammer
-		{ "26573", { -- consecration glyphed
-			"player.glyph(54928)", 
-			"modifier.alt" 
-		}, "ground"},
+		{ "26573", { "player.glyph(54928)", "modifier.alt" }, "ground"}, -- consecration glyphed
 
-	-- seals
-		{ "20165", { -- seal of Insight
-			"player.seal != 3",
-			"!modifier.multitarget",
-			"@mtsPalaProt.Getseal()"
-		}, nil },
-		{ "20154", { -- seal of righteousness
-			"player.seal != 2",
-			"modifier.multitarget",
-			"@mtsPalaProt.Getseal()"
-		}, nil },
+	-- Seals
+		{ "20165", { "player.seal != 3", "!modifier.multitarget", "@mts.GetSeals()" }, nil }, -- seal of Insight
+		{ "20154", { "player.seal != 2", "modifier.multitarget", "@mts.GetSeals()" }, nil }, -- seal of righteousness
 
 	-- Hands
-		{ "6940", { "lowest.health <= 80", "!player.health <= 40" }, "lowest" }, -- Hand of Sacrifice
+		{ "6940", { "lowest.health <= 80", "!player.health <= 40", "!lowest.player" }, "lowest" }, -- Hand of Sacrifice
 		{ "1044", "player.state.root" }, -- Hand of Freedom
+		{ "1022", { "!lowest.role(tank)", "!lowest.immune.melee", "lowest.health < 25" }, "lowest" }, -- Hand of Protection
 		
 	-- Interrupt
-		{ "96231", "modifier.interrupts"}, -- Rebuke
+		{ "96231", "modifier.interrupts" }, -- Rebuke
 
-	{{-- Defensive Cooldowns
-		{ "20925", "!player.buff(20925)", "player" }, -- Sacred Shield 		
-		{ "31850", "player.health < 30" }, --Ardent Defender
-		{ "498", "player.health <= 99" }, -- Divine Protection
-		{ "86659", "player.health <= 50" }, -- Guardian of Ancient Kings
-		{ "#gloves" },
-	}, "toggle.defcd" },
+	-- Defensive Cooldowns
+		{ "20925", { "!player.buff(20925)", "toggle.defcd" }, "player" }, -- Sacred Shield 		
+		{ "31850", { "player.health < 30", "toggle.defcd" }, nil }, --Ardent Defender
+		{ "498", { "player.health <= 99", "toggle.defcd" }, nil }, -- Divine Protection
+		{ "86659", { "player.health <= 50", "toggle.defcd" }, nil }, -- Guardian of Ancient Kings
+		{ "#gloves", "toggle.defcd", nil },
 	
 	-- Cooldowns
 		{ "31884", "modifier.cooldowns" }, -- Avenging Wrath
 		{ "105809", "modifier.cooldowns" }, --Holy Avenger
-		
+	
+	-- Items
+		{ "#5512", "player.health < 60" }, --Healthstone
+		{ "#76097", { "player.health < 30", "@mts.HealthPot" }, nil }, -- Master Health Potion
+		--{ "#86125", { "@mts.KafaPress", "modifier.cooldowns" }, nil }, -- Kafa Press // Not working right... //
+
 	-- Self Heal
-		{ "#5512", "player.health <= 60" }, --Healthstone
-		{ "633", "player.health <= 20", "player"}, -- Lay on Hands
-		{ "114163", { -- Eternal Flame
-			"!player.buff(114163)",
-			"player.buff(114637).count = 5",
-			"player.holypower >= 3",
-			"player.health <= 85"
-		}, "player"},
-		{ "85673", {  -- Word of Glory
-			"player.buff(114637).count = 5",
-			"player.holypower >= 3",
-			"player.health <= 40"
-		}, "player" },
+		{ "633", "player.health < 20", "player"}, -- Lay on Hands
+		{ "114163", { "!player.buff(114163)", "player.buff(114637).count = 5", "player.holypower >= 3", "player.health < 60" }, "player"}, -- Eternal Flame
+		{ "114163", { "player.holypower >= 1", "player.health < 30" }, "player"}, -- Eternal Flame / Low
+		{ "85673", { "player.buff(114637).count = 5", "player.holypower >= 3", "player.health < 60" }, "player" }, -- Word of Glory
+		{ "85673", { "player.holypower >= 1", "player.health < 30" }, "player" }, -- Word of Glory / Low
+		{ "19750", { "player.health < 70", "player.buff(114250).count > 2", "player.buff(114637" }, "player" }, -- Flash of Light / Selfless Healer / Bastion of glory / Player
+
+	-- Raid Heal
+		{ "19750", { "lowest.health < 50", "player.buff(114250).count > 2" }, "lowest" }, -- Flash of Light / Selfless Healer / Lowest
+        { "114163", { "player.holypower >= 1", "player.health < 30" }, "Lowest"}, -- Eternal Flame
+        { "85673", { "player.holypower >= 1", "player.health < 30" }, "lowest" }, -- Word of Glory 
+
+    -- Auto Target
+    	{ "/script TargetNearestEnemy()", { "toggle.autotarget", "!target.exists" }, nil },
+        { "/script TargetNearestEnemy()", { "toggle.autotarget", "target.exists", "target.dead" }, nil },
+
+    -- Taunts
+		{ "62124", { "toggle.aggro", "@mts.bossTaunt()" }, target }, -- Boss // Reckoning
+		--{ "62124", { "toggle.aggro", "target.threat < 100", "@mts.StopIfBoss()" }, target }, -- Aggro Control // Reckoning
+		--{ "62124", { "toggle.aggro", "mouseover.threat < 100", "@mts.StopIfBoss()" }, mouseover }, -- Aggro Control // Reckoning
 
 	-- Rotation
-
-		{ "24275", { -- Hammer of Wrath
-			"target.health <= 20",
-			"target.spell(24275).range"
-		}, "target" },
-		{ "31935", { -- Avenger´s Shield Pro
-			"player.buff(98057)",
-			"target.spell(31935).range"
-		}, "target" },
-		{ "53600", { -- Shield of the Righteous
-			"player.holypower >= 3", 
-			"target.spell(53600).range" 
-		}, "target" },
-		{ "35395", { -- Crusader Strike
-			"!modifier.multitarget", 
-			"target.spell(35395).range" 
-		}, "target" },
-		{ "53595", { -- Hammer of the Righteous
-			"modifier.multitarget", 
-			"target.spell(53595).range" 
-		}, "target" },
+		{ "24275", { "target.health <= 20", "target.spell(24275).range" }, "target" }, -- Hammer of Wrath
+		{ "31935", { "player.buff(98057)", "target.spell(31935).range" }, "target" }, -- Avenger´s Shield Proc
+		{ "53600", { "player.holypower >= 3", "target.spell(53600).range" }, "target" }, -- Shield of the Righteous
+		{ "35395", { "!modifier.multitarget", "target.spell(35395).range" }, "target" }, -- Crusader Strike
+		{ "53595", { "modifier.multitarget", "target.spell(53595).range" }, "target" }, -- Hammer of the Righteous
 		{ "20271", "target.spell(20271).range", "target" }, -- Judgment
 		{ "114165", "target.spell(114165).range", "target" }, -- Holy Prism
 		{ "31935", "target.spell(31935).range", "target" },-- Avenger´s Shield Normal
-		{ "26573", { -- consecration
-			"!player.glyph(54928)", 
-			"target.range <= 5", 
-			"@mtsPalaProt.GetCon()" 
-		}, nil },
+		{ "26573", { "!player.glyph(54928)", "target.range <= 5", "@mts.GetCon()" }, nil }, -- consecration
 		{ "114157", "target.spell(114157).range", "target" }, -- Execution Sentense
 		{ "119072" }, -- Holy Wrath
   
-} -- //////////////////////-----------------------------------------END IN-COMBAT-----------------------------------//////////////////////////////
+}
+
+-- //////////////////////-----------------------------------------END IN-COMBAT-----------------------------------//////////////////////////////
 
 local outCombat = {
 
@@ -281,27 +304,18 @@ local outCombat = {
 		{ "105593", "modifier.control", "target"}, -- Fist of Justice
 		{ "853", "modifier.control", "target"}, -- Hammer of Justice
 		{ "114158", "modifier.shift", "ground"}, -- Light´s Hammer
-		{ "26573", { -- consecration glyphed
-			"player.glyph(54928)", 
-			"modifier.alt" 
-		}, "ground"},
+		{ "26573", { "player.glyph(54928)", "modifier.alt" }, "ground"}, -- consecration glyphed
 
 	-- seals
-		{ "20165", { -- seal of Insight
-			"player.seal != 3",
-			"!modifier.multitarget",
-			"@mtsPalaProt.Getseal()"
-		}, nil },
-		{ "20154", { -- seal of righteousness
-			"player.seal != 2",
-			"modifier.multitarget",
-			"@mtsPalaProt.Getseal()"
-		}, nil },
+		{ "20165", { "player.seal != 3", "!modifier.multitarget", "@mts.GetSeals()" }, nil }, -- seal of Insight
+		{ "20154", { "player.seal != 2", "modifier.multitarget", "@mts.GetSeals()" }, nil }, -- seal of righteousness
 
 	-- Hands
 		{ "1044", "player.state.root" }, -- Hand of Freedom
 
-}-- //////////////////////-----------------------------------------END OUT-OF-COMBAT-----------------------------------//////////////////////////////
+}
+
+-- //////////////////////-----------------------------------------END OUT-OF-COMBAT-----------------------------------//////////////////////////////
 
 for _, Buffs in pairs(Buffs) do
   inCombat[#inCombat + 1] = Buffs
