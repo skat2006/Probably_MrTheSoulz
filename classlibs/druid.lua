f26e36da3e5740e377c7fb956fc230313a7d7d61
Jaime Marques <mrthesoulz@gmail.com>
2014-02-05 18:56:40 +0000

    Forgot druid

--- classlibs/druid.lua
+++ classlibs/druid.lua
@@ -5,39 +5,4 @@ I Hope Your Enjoy Them
 MTS
 ]]
 
--- Made By Tao
-local ignoreDebuffs = {
-  'Mark of Arrogance',
-  'Displaced Energy'
-}
-
-function mtsLibDood.druid()
-local prefix = (IsInRaid() and 'raid') or 'party'
-	for i = -1, GetNumGroupMembers() - 1 do
-	local unit = (i == -1 and 'target') or (i == 0 and 'player') or prefix .. i
-		if IsSpellInRange("Nature's Cure", unit) then
-			for j = 1, 40 do
-			local debuffName, _, _, _, dispelType, duration, expires, _, _, _, spellID, _, isBossDebuff, _, _, _ = UnitDebuff(unit, j)
-				if dispelType and dispelType == 'Magic' or dispelType == 'Poison' or dispelType == 'Disease' then
-				local ignore = false
-				for k = 1, #ignoreDebuffs do
-					if debuffName == ignoreDebuffs[k] then
-						ignore = true
-						break
-					end
-				end
-					if not ignore then
-						ProbablyEngine.dsl.parsedTarget = unit
-						return true
-					end
-				end
-				if not debuffName then
-					break
-				end
-			end
-		end
-	end
-		return false
-end
-
-ProbablyEngine.library.register('mtsLibDood', mtsLibDood)
+--ProbablyEngine.library.register('mtsLibDood', mtsLibDood)
