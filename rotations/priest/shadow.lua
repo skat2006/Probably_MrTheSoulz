fe678301eddbb59ee3e08e88e9813a962bbf6cf1
Jaime Marques <mrthesoulz@gmail.com>
2014-10-22 12:48:59 +0100

    woot? ops

--- rotations/priest/shadow.lua
+++ rotations/priest/shadow.lua
@@ -1,54 +1,59 @@
--- SPEC ID 258
--- 7/11/2013 - Changed by j
-ProbablyEngine.rotation.register(258, {
 
-  --------------------
-  -- Start Rotation --
-  --------------------
+local lib = function()
+
+  mtsStart:message("\124cff9482C9*MrTheSoulz - \124cffABD473Priest/Shadow \124cff9482C9Loaded*")
+
+end
+
+local Shared = {
   
+  --buffs
+    { "Power Word: Fortitude", "!player.buff(Power Word: Fortitude)" },
+    { "Inner Fire", "!player.buff(Inner Fire)" },
+    { "Shadow Form", "!player.buff(Shadowform)" },
+
+}
+
+local inCombat = {
+
   -- Cooldowns
-  { "Power Infusion", "modifier.cooldowns" },
-  { "Shadowfiend", "modifier.cooldowns" },
+    { "Power Infusion", "modifier.cooldowns" },
+    { "Shadowfiend", "modifier.cooldowns" },
     
   -- Keybinds
-  { "Mind Sear", "modifier.shift" },
+    { "Mind Sear", "modifier.shift" },
     
   -- If Moving
-  { "Shadow Word: Pain", "player.moving" },
-  { "Cascade", "player.moving" },
-  { "Halo", "player.moving" },
-  { "Shadow Word: Death", "player.moving" },
+    { "Shadow Word: Pain", "player.moving" },
+    { "Cascade", "player.moving" },
+    { "Halo", "player.moving" },
+    { "Shadow Word: Death", "player.moving" },
     
   -- Rotation
-  { "Mind Blast", "player.buff(Divine Insight)" },
-  { "Devouring Plague", "player.shadoworbs = 3" },
-  { "Mind Blast" }, 
-  { "Shadow Word: Death", "target.debuff(Shadow Word: Death).duration < 1" },
-  { "Shadow Word: Pain", "target.debuff(Shadow Word: Pain).duration < 3" },
-  { "Vampiric Touch", "target.debuff(Vampiric Touch).duration < 1" },
-  { "Mind Flay", "target.debuff(Devouring Plague)" }, 
-  { "Cascade", },
-  { "Halo" },
-  { "Mind Spike", "player.buff(Surge of Darkness)" },
-  { "Mind Flay" },
-  { "Shadow Word: Death" },
+    { "Shadow Word: Pain", "target.debuff(Shadow Word: Pain).duration <= 5" },
+    { "Mind Blast", "player.buff(Divine Insight)" },
+    { "Devouring Plague", "player.shadoworbs >= 3" },
+    { "Mind Blast" }, 
+    { "Shadow Word: Death", "target.debuff(Shadow Word: Death).duration < 1" },
+    { "Vampiric Touch", "target.debuff(Vampiric Touch).duration <= 4" },
+    { "Mind Flay", "target.debuff(Devouring Plague)" }, 
+    { "Cascade", },
+    { "Halo" },
+    { "Mind Spike", "player.buff(Surge of Darkness)" },
+    { "Mind Flay" },
+    { "Shadow Word: Death" },
   
-  ------------------
-  -- End Rotation --
-  ------------------
+} 
+
+local outCombat = {
+
+
   
-  }, {
-  
-  ---------------
-  -- OOC Begin --
-  ---------------
-  
-  { "Power Word: Fortitude", "!player.buff(Power Word: Fortitude)" },
-  { "Inner Fire", "!player.buff(Inner Fire)" },
-  { "Shadow Form", "!player.buff(Shadowform)" },
-  
-  -------------
-  -- OOC End --
-  -------------
-  
-  })
+}
+
+for _, Shared in pairs(Shared) do
+  inCombat[#inCombat + 1] = Shared
+  outCombat[#outCombat + 1] = Shared
+end
+
+ProbablyEngine.rotation.register_custom(258, "|r[|cff9482C9MTS|r][Testing Priest-Shadow|r]", inCombat, outCombat, lib)
