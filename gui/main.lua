c846e588769c5443a896b03b53a41edc9ca90eef
Jaime Marques <mrthesoulz@gmail.com>
2014-10-29 18:45:33 +0000

    bump

--- gui/main.lua
+++ gui/main.lua
@@ -7,7 +7,7 @@ I Hope Your Enjoy Them
 MTS
 ]]
 
-local mts_VERSION = "\124cff9482C9*[MTS] \124cffFFFFFFVersion: 0.8.10\124cff9482C9*"
+local mts_VERSION = "\124cff9482C9*[MTS] \124cffFFFFFFVersion: 0.8.12\124cff9482C9*"
 local mConfigVersion = 1
 if M_CONFIG_VERSION == nil or MCONFIG_VERSION < mConfigVersion then
 local DISPLAYED_OPTIONS = 10
