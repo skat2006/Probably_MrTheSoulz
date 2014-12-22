472215d11bba565d6f37be73fcb40d9770a2df13
Jaime Marques <mrthesoulz@gmail.com>
2014-12-22 22:04:24 +0000

    Fury

--- rotations/warrior/fury.lua
+++ rotations/warrior/fury.lua
@@ -1,9 +1,7 @@
 --[[ ///---INFO---////
-// Warrior Fury //
-Thank You For Using My ProFiles
-I Hope Your Enjoy Them
-MTS
-]]
+Fury Warrior DPS is based on maintaining a high uptime on Enrage to generally buff 
+  your damage while also using as many Execute as possible. 
+  ///---INFO---////  ]]
 
 local fetch = ProbablyEngine.interface.fetchKey
 
@@ -14,60 +12,52 @@ mts_Splash("|cff9482C9[MTS]-|cffFFFFFF"..(select(2, GetSpecializationInfo(GetSpe
 	
 end
 
-local Shared = {
-	
-	-- Buffs
-		{ "6673", "!player.buff(6673)" }, -- Battle Shout
+local All = {
 
-	-- Stances
-		{ "2487", "player.seal != 1", nil  }, -- Battle Stance
-		
+		{ "6673", "!player.buff(6673)" }, 						-- Battle Shout
+		{ "2487", "player.seal != 1", nil  }, 					-- Battle Stance
+		{ "6544", "modifier.shift", "mouseover.ground" }, 		-- Heroic Leap // FH
+	  	{ "5246", "modifier.control" }, 						-- Intimidating Shout
+		{ "100", { 												-- Charge
+			"modifier.alt", 
+			"target.spell(100).range" 
+		}, "target"},
 		
 }
 
-local inCombat = {
+local racials = {
+	
+	-- Dwarves
+	{ "20594", "player.health <= 65" },
+	-- Humans
+	{ "59752", "player.state.charm" },
+	{ "59752", "player.state.fear" },
+	{ "59752", "player.state.incapacitate" },
+	{ "59752", "player.state.sleep" },
+	{ "59752", "player.state.stun" },
+	-- Draenei
+	{ "28880", "player.health <= 70", "player" },
+	-- Gnomes
+	{ "20589", "player.state.root" },
+	{ "20589", "player.state.snare" },
+	-- Forsaken
+	{ "7744", "player.state.fear" },
+	{ "7744", "player.state.charm" },
+	{ "7744", "player.state.sleep" },
+	-- Goblins
+	{ "69041", "player.moving" },
 
-	--Racials
-        -- Dwarves
-			{ "20594", "player.health <= 65" },
-		-- Humans
-			{ "59752", "player.state.charm" },
-			{ "59752", "player.state.fear" },
-			{ "59752", "player.state.incapacitate" },
-			{ "59752", "player.state.sleep" },
-			{ "59752", "player.state.stun" },
-		-- Draenei
-			{ "28880", "player.health <= 70", "player" },
-		-- Gnomes
-			{ "20589", "player.state.root" },
-			{ "20589", "player.state.snare" },
-		-- Forsaken
-			{ "7744", "player.state.fear" },
-			{ "7744", "player.state.charm" },
-			{ "7744", "player.state.sleep" },
-		-- Goblins
-			{ "69041", "player.moving" },
+}
 
-	-- Freedoom
-		{ "7744", "player.state.fear" }, -- Will of the Forsaken
-  		{ "7744", "player.state.charm" },
-  		{ "7744", "player.state.sleep" },
-		
-	-- keybinds
-		{ "6544", "modifier.shift", "mouseover.ground" }, -- Heroic Leap // FH
-  		{ "5246", "modifier.control" }, -- Intimidating Shout
-		{ "100", { "modifier.alt", "target.spell(100).range" }, "target"}, -- Charge
-
-	-- Interrupt
-		{ "6552", {"target.interruptsAt(50)", "modifier.interrupts"}, "target" }, -- Pummel
-  		{ "114028", {"target.interruptsAt(50)","modifier.interrupts"}, "target" }, -- Mass Spell Reflection
+local Cooldowns = {
 	
 	-- Cooldowns
-		{ "12292", "modifier.cooldowns" },--Bloodbath
-  		{ "107574", "modifier.cooldowns" },-- Avatar
-  		{ "107570", "modifier.cooldowns" }, -- Storm Bolt
-  		{ "1719", "modifier.cooldowns" }, -- Recklessness
-  		{ "46924", "modifier.cooldowns" }, -- Bladestorm
+		{ "12292" },			-- Bloodbath
+  		{ "107574" },			-- Avatar
+  		{ "107570" }, 			-- Storm Bolt
+  		{ "1719" }, 			-- Recklessness Use as often as possible. Stack with other DPS cooldowns.
+  		{ "46924" }, 			-- Bladestorm
+  		{ "Berserker Rage" },	-- Berserker Rage Use as needed to maintain Enrage.
   		{ "#gloves" },
 	
 	--[[ Items
@@ -75,57 +65,80 @@ local inCombat = {
 		{ "#76097", "player.health < 30", "@mtsLib.checkItem(HealthPot)" }, -- Master Health Potion
 		{ "#86125", { "modifier.cooldowns","@mtsLib.checkItem(KafaPress)" }}, -- Kafa Press]]
 
-    -- Auto Target
-    	{ "/cleartarget", (function() return UnitIsFriend("player","target") end) },
-    	{ "/TargetNearestEnemy", "!target.exists" },
-        { "/TargetNearestEnemy", { "target.exists", "target.dead" } },
-	
-    -- Survival
-  		{ "97462", { "player.health < 15", "modifier.cooldowns" }}, -- Rallying Cry
-  		{ "118038", "player.health < 25" }, -- Die by the Sword
-
-  	-- Proc's
-  		{ "5308", "player.buff(29725)", "target" }, -- Execute, Sudden Death
-  		{ "100130", "player.buff(46916)", "target" }, -- Wild Strike, Bloodsurge
-  		{ "34428" }, -- Victory Rush
-
-		{{-- can use FH
-
-	    	-- AoE smart
-	     	{ "1680", "player.area(8).enemies > 3" }, -- Whirlwind
-
-	  	}, (function() return fetch('mtsconf','Firehack') end) },
-
-
-	-- AoE
-		{ "1680", "modifier.multitarget" }, -- Whirlwind
-
-	-- Rotaion Execute (Target as less then 20% health)
-		{ "5308", {"player.rage > 80", "target.health <= 20"}, "target" }, -- Execute
-		{ "23881", "target.health <= 20", "target" }, -- Bloodthirst
-		{ "5308", {"player.rage >= 60", "target.health <= 20"}, "target" }, -- Execute
-
-	-- Rotation normal
-		{ "100130", "player.rage > 80", "target" }, -- Wild Strike
-		{ "23881" }, -- Bloodthirst
-		{ "85288" }, -- Raging Blow
-		
-	-- Ranged
-		{ "57755", "player.range > 10", "target" } -- Heroic Throw
 }
 
-local outCombat = {
+local Survival = {
 	
-	-- keybinds
-		{ "6544", "modifier.shift", "mouseover.ground" }, -- Heroic Leap // FH
-  		{ "5246", "modifier.control" }, -- Intimidating Shout
-		{ "100", { "modifier.alt", "target.spell(100).range" }, "target"}, -- Charge
+	{ "97462", "player.health < 15" }, 		-- Rallying Cry
+  	{ "118038", "player.health < 25" }, 	-- Die by the Sword
 
 }
 
-for _, Shared in pairs(Shared) do
-  inCombat[#inCombat + 1] = Shared
-  outCombat[#outCombat + 1] = Shared
-end
+--[[ ///---INFO---////
+With 3 or less targets, you should use Bloodthirst on cooldown to maintain Enrage. 
+Use Whirlwind with >= 80 Rage and to stack the Raging Blow buff. 
+Use Raging Blow when there are 2-3 stacks of the buff. 
+Use Wild Strike as needed to consume Bloodsurge procs. 
+With 4+ targets, place a higher emphasis on Whirlwind and continue to use it to dump Rage even with max Raging Blow stacks.
+  ///---INFO---////  ]]
+local AoE = {
+	
+	{ "1680" }, -- Whirlwind
 
-ProbablyEngine.rotation.register_custom(72, mts_Icon.."|r[|cff9482C9MTS|r][|cffF58CBAWarrior-Fury|r]", inCombat, outCombat, exeOnLoad)
+}
+
+--[[ ///---INFO---////
+The Execute priorty is similar to the Normal priority but places more emphasis on the use of Execute. 
+To follow this priority, use Execute instead of Wild Strike to prevent capping your Rage. 
+Continue to use Bloodthirst on cooldown when not Enraged in order to proc Enrage and Bloodsurge. 
+Now, use Execute while Enrage or whenever you go above 60 Rage. 
+Finally, continue to use Wild Strike while Enrage or to consume Bloodsurge procs.
+  ///---INFO---////  ]]
+local Execute = {
+	
+	{ "5308", "player.rage => 80", "target" }, 		-- Execute to prevent capping your Rage.
+	{ "23881" }, 									-- Bloodthirst on cooldown to maintain Enrage. Procs Bloodsurge.
+	{ "5308", "player.rage => 60", "target" }, 		-- Execute while Enrage.
+	{ "5308", "player.buff(Enraged)", "target" }, 	-- Execute with >= 60 Rage.
+	{ "100130", "player.buff(Enraged)", "target" }, -- Wild Strike while Enrage or with Bloodsurge procs.
+
+}
+
+--[[ ///---INFO---////
+The Normal rotation should be followed while your target is above 20% health and 
+  then you should switch to the Exectue rotation when your target drops below 20% health.
+The Normal priority uses Wild Strike when necessary to prevent capping your Rage. 
+After this, use Bloodthirst on cooldown when not Enraged in order to try and proc Enrage as well as Bloodsurge. 
+Next, use Raging Blow whenever available. 
+Also, use Wild Strike when Enraged or to consume Bloodsurge procs.
+  ///---INFO---////  ]]
+local Normal = {
+
+	{ "100130", "player.rage > 80", "target" },	 	-- Wild Strike to prevent capping your Rage.
+	{ "23881", "!player.buff(Enraged)", "target" }, -- Bloodthirst on cooldown when not Enraged. Procs Bloodsurge.
+	{ "85288" }, 									-- Raging Blow when available.
+	{ "100130", "player.buff(Enraged)", "target" }, -- Wild Strike when Enraged.
+	{ "57755", "player.range > 10", "target" } 		-- Heroic Throw
+
+}
+
+
+ProbablyEngine.rotation.register_custom(72, mts_Icon.."|r[|cff9482C9MTS|r][|cffF58CBAWarrior-Fury|r]", 
+	{-- Incombat
+		{ "6552", "target.interruptsAt(50)", "target" }, 		-- Pummel
+	  	{ "114028", "target.interruptsAt(50)", "target" }, 		-- Mass Spell Reflection
+	  	{ "5308", "player.buff(29725)", "target" }, 			-- Proc // Execute, Sudden Death
+	  	{ "100130", "player.buff(46916)", "target" },			-- Proc // Wild Strike, Bloodsurge
+	  	{ "34428" }, 											-- Victory Rush
+		{ All },
+		{ Survival },
+		{ Cooldowns, "modifier.cooldowns" },
+		{ Execute, "target.health <= 20" },
+		{ AoE, "modifier.multitarget" },
+		{ AoE, "player.area(8).enemies >= 3" },
+		{ Normal }
+	}, 
+	{ -- Out Combat
+		{ All }
+	}, 
+	exeOnLoad)
