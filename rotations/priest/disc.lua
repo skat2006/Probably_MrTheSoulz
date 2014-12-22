3946b3452e145e476647237016462a6f5f5929d3
Jaime Marques <mrthesoulz@gmail.com>
2014-12-22 17:21:55 +0000

    More

--- rotations/priest/disc.lua
+++ rotations/priest/disc.lua
@@ -1,10 +1,3 @@
---[[ ///---INFO---////
-//Priest Disc//
-Thank You For Using My ProFiles
-I Hope Your Enjoy Them
-MTS
-]]--
-
 local fetch = ProbablyEngine.interface.fetchKey
 
 local ignoreDebuffs = {
@@ -183,6 +176,29 @@ local AoE = {
 
 }
 
+local FlashHeal = {
+	
+	{ "!2061", {
+		(function() return mts_dynamicEval("focus.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealTank')) end),
+		"focus.spell(2061).range",
+		"!player.moving"
+	}, "focus" },
+	{ "!2061", {
+		(function() return mts_dynamicEval("tank.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealTank')) end), 
+		"tank.spell(2061).range",
+		"!player.moving"
+	}, "tank" },
+	{ "!2061", {
+		(function() return mts_dynamicEval("player.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealPlayer')) end),
+			"!player.moving"
+	}, "player" },
+		{ "!2061", {
+			(function() return mts_dynamicEval("lowest.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealRaid')) end),
+			"!player.moving"
+	}, "lowest" },
+
+}
+
 local Normal = {
 
 	-- Penance	
@@ -252,48 +268,8 @@ local Solo = {
 		}, "modifier.multitarget" },
 	}, {"toggle.dotEverything"} },
 
-   	-- buffs
-		{ "81700", "player.buff(81661).count = 5" }, -- Archangel
-
-  	-- Mana/Survival
-		{ "123040", { --Mindbender
-			"player.mana < 75",
-			"target.spell(123040).range"
-		}, "target" },
-		{ "34433", { --Shadowfiend
-			"player.mana < 75",
-			"target.spell(34433).range"
-		}, "target" },
-		
-		{ "19236", "player.health <= 20", "Player" }, --Desperate Prayer
-
-  	-- HEALTHSTONE 
-		{ "#5512", "player.health <= 35" },
-
-  	-- Aggro
-		{ "586", "target.threat >= 80" }, -- Fade
- 
-  	-- Dispel's
-	 	{{ -- Dispell all?
-			{ "527", (function() return Dispell() end) },-- Dispel Everything
-		}, (function() return fetch('mtsconfPriestDisc','Dispels') end) },
-
   	-- CD's
-		{ "10060", "modifier.cooldowns" }, --Power Infusion
-	
-	-- For Archangel
-		{ "14914", { --Holy Fire
-			"player.mana > 20",
-			"target.spell(14914).range",
-			"target.infront" 
-		}, "target" }, 
-
-	-- Surge of light
-		{ "2061", {-- Flash Heal
-			"lowest.health < 100",
-			"player.buff(114255)",
-			"!player.moving"
-		}, "lowest" }, 
+		{ "10060", "modifier.cooldowns" }, --Power Infusion 
 
 	-- Flash Heal
 		{ "2061", { --Flash Heal
@@ -308,34 +284,47 @@ local Solo = {
 			"!player.buff(17).any" 
 		}, "player" },
 
-	--Attonement 
-		{ "47540", { --Penance
-			"player.mana > 20", 
-			"target.spell(47540).range", 
-			"!player.moving",
-			--"target.infront"
-		}, "target" },
-		{ "585", {  --Smite
-			"player.mana > 20", 
-			"!player.moving", 
-			"target.spell(585).range",
-			--"target.infront"
-		}, "target" },
-
 }
 
 local outCombat = {
 	
-	-- buffs
-		{ "21562", {-- Fortitude
-			"buffs.stamina"
-			}}, 
-		
-		{ "81700", {--Archangel
-			"player.buff(81661).count = 5", 
-			"player.buff(81661).duration < 5"
-			}},
+	-- Heals 
+		{ "!47540", { --Penance
+			(function() return mts_dynamicEval("lowest.health <= " .. fetch('mtsconfPriestDisc', 'PenanceRaid')) end),
+			"!player.casting(2061)",
+			"!player.moving"
+			}, "lowest" },
+		{ "2060", {-- Heal
+			(function() return mts_dynamicEval("lowest.health <= " .. fetch('mtsconfPriestDisc', 'HealRaid')) end),
+			"!player.moving"
+			}, "lowest" }, 
+
+}
+
+local All = {
+
+	-- Key Binds
+		{ "48045", "modifier.alt", "target" }, -- Mind Sear
 	
+	-- buffs
+		{ "21562", "!player.buffs.stamina" }, -- Fortitude
+		{ "81700", "player.buff(81661).count = 5" }, -- Archangel
+	
+	{{-- Dispell ALl // Dont interrumpt if castbar more then 50%
+		{ "!527", (function() return Dispell() end) },-- Dispel Everything
+	}, "!player.casting.percent >= 50", (function() return fetch('mtsconfPriestDisc','Dispels') end) },
+
+	-- Sulvival
+	{ "19236", "player.health <= 20", "player" }, --Desperate Prayer
+	{ "#5512", "player.health <= 35" }, -- Health Stone
+	{ "586", "target.threat >= 80" }, -- Fade
+		-- Surge of light
+		{ "2061", {-- Flash Heal
+			"lowest.health < 100",
+			"player.buff(114255)",
+			"!player.moving"
+		}, "lowest" },
+
 	{{-- LoOk aT It GOoZ!!!
 		{ "121536", { 
 			"player.movingfor > 2", 
@@ -354,94 +343,15 @@ local outCombat = {
 			end
 		end)  
 	},
-	
-	{{-- AOE
-		{ "121135", { -- cascade
-			"@coreHealing.needsHealing(95, 3)", 
-				"!player.moving"
-		}, "lowest"},
- 		{ "596", "@mtsLib.PoH" },-- Prayer of Healing
-   		{ "132157", "@mtsLib.holyNova", nil }, -- Holy Nova
-	}, "modifier.multitarget" },
-	
-	-- Heals
-		-- Surge of light
-		{ "2061", {-- Flash Heal
-			"lowest.health < 100",
-			"player.buff(114255)",
-			"!player.moving"
-		}, "lowest" }, 
-		{ "!47540", { --Penance
-			(function() return mts_dynamicEval("lowest.health <= " .. fetch('mtsconfPriestDisc', 'PenanceRaid')) end),
-			"!player.casting(2061)",
-			"!player.moving"
-			}, "lowest" },
-		{ "2060", {-- Heal
-			(function() return mts_dynamicEval("lowest.health <= " .. fetch('mtsconfPriestDisc', 'HealRaid')) end),
-			"!player.moving"
-			}, "lowest" }, 
 
 }
 	
 ProbablyEngine.rotation.register_custom(
 	256, 
 	mts_Icon.."|r[|cff9482C9MTS|r][|cffFFFFFFPriest-Disc-Party|r]", 
-	{-- Dyn Change CR
-		{{--Party/Raid CR
-			-- Keybinds
-			{ "32375", "modifier.control", "player.ground" }, --Mass Dispel
-		 	{ "48045", "modifier.alt", "target" }, -- Mind Sear
-		 	{{-- Dont interrumpt if castbar more then 50%
-			 	-- Dispell ALl
-				{ "!527", (function() return Dispell() end) },-- Dispel Everything
-			}, "!player.casting.percent >= 50", (function() return fetch('mtsconfPriestDisc','Dispels') end) },
-			{ Cooldowns, "modifier.cooldowns" },
-			-- Surge of light
-			{ "2061", {-- Flash Heal
-				"lowest.health < 100",
-				"player.buff(114255)",
-				"!player.moving"
-			}, "lowest" },
-			-- buffs
-			{ "81700", "player.buff(81661).count = 5" }, -- Archangel
-			{{-- Flash Heal // dont interrumpt if castbar more then 50%
-				{ "!2061", {
-					(function() return mts_dynamicEval("focus.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealTank')) end),
-					"focus.spell(2061).range",
-					"!player.moving"
-				}, "focus" },
-				{ "!2061", {
-					(function() return mts_dynamicEval("tank.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealTank')) end), 
-					"tank.spell(2061).range",
-					"!player.moving"
-				}, "tank" },
-				{ "!2061", {
-					(function() return mts_dynamicEval("player.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealPlayer')) end),
-					"!player.moving"
-				}, "player" },
-				{ "!2061", {
-					(function() return mts_dynamicEval("lowest.health <= " .. fetch('mtsconfPriestDisc', 'FlashHealRaid')) end),
-					"!player.moving"
-				}, "lowest" },
-			}, "!player.casting.percent >= 50" },
-			{{-- LoOk aT It GOoZ!!!
-				{ "121536", { 
-					"player.movingfor > 2", 
-					"!player.buff(121557)", 
-					"player.spell(121536).charges >= 1" 
-				}, "player.ground" },
-				{ "17", {
-					"talent(2, 1)", 
-					"player.movingfor > 2", 
-					"!player.buff(6788)",
-				}, "player" },
-			}, -- We only want to run these on unlockers that can cast on unit.ground
-				(function()
-					if FireHack or oexecute then
-						return fetch('mtsconfPriestDisc', 'Feathers') 
-					end
-				end)  
-			},
+	{-- Dyn In-Combat Change CR
+		 	
+		 	{ All },
 		 	-- Mana
 		 	{ "123040", { --Mindbender
 				"player.mana < 75",
@@ -451,11 +361,25 @@ ProbablyEngine.rotation.register_custom(
 				"player.mana < 75",
 				"target.spell(34433).range"
 			}, "target" },
-			{ "19236", "player.health <= 20", "player" }, --Desperate Prayer
-			{ "#5512", "player.health <= 35" }, -- Health Stone
-			{ "586", "target.threat >= 80" }, -- Fade
+		
+		{{--Party/Raid CR
+			
+			-- Keybinds
+			{ "32375", "modifier.control", "player.ground" }, --Mass Dispel
+			
+			-- Cooldowns
+			{ Cooldowns, "modifier.cooldowns" },
+			
+			-- Flash Heal // dont interrumpt if castbar more then 50%
+			{ FlashHeal, "!player.casting.percent >= 50" },
+		 	
+		 	-- BorrowedTime // Passive Buff
 		 	{ BorrowedTime, "player.buff(59889).duration <= 2" },
+		 	
+		 	-- SpiritShell // Talent
 		 	{ SpiritShell, "player.buff(109964)" },
+		 	
+		 	-- Attonement
 		 	{ Attonement, {
 		 		(function() return mts_dynamicEval("lowest.health >= " .. fetch('mtsconfPriestDisc', 'Attonement')) end),
 				(function() return mts_infront('target') end),
@@ -463,10 +387,24 @@ ProbablyEngine.rotation.register_custom(
 				"!player.mana < 20",
 				"target.range <= 30",
 		 	}},
+		 	
+			-- AoE Heals
 		 	{ AoE, "modifier.multitarget" },
+			
+			-- Normal Heals
 			{ Normal}
+		
 		}, "modifier.party" },
-		-- Solo CR
-		{ Solo, "!modifier.party" },
+		
+		{{-- Solo CR
+			{ FlashHeal, "!player.casting.percent >= 50" },
+			{ Solo },
+			{ Attonement },
+		}, "!modifier.party" },
+
 	},  
-	outCombat, exeOnLoad)
+	{ -- Dyn Out-Combat Change CR
+		{ All },
+		{ FlashHeal, "!player.casting.percent >= 50" },
+		{outCombat}
+	}, exeOnLoad)
